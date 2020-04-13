cb45484c66b47d2d2965a49c798305af0086ef68
jcseeco <dontbirdanyone@gmail.com>
2020-04-13 15:56:06 +0800

    update api use

--- js/ajaxHandler.js
+++ js/ajaxHandler.js
@@ -1,11 +1,15 @@
 // for handling api requests
 class AjaxHandler {
   constructor() {
-
+    this.classesUrl = "https://test7api.baodao7.com/web/classes/list";
+    this.classUrl = "https://test7api.baodao7.com/web/classes";
+    this.lessionsUrl = "https://test7api.baodao7.com/web/class_details/list";
+    this.lessionUrl = "https://test7api.baodao7.com/web/class_details";
+    this.teachersUrl = "https://test7api.baodao7.com/web/teacher/list";
   }
 
   // 取得課程列表
-  getClasses(teacher_id = false, cat_id = false, order_column = 'updated_at', order_type = 'desc', offset = 0) {
+  async getClasses(cat_id = false, teacher_id = false, order_column = 'updated_at', order_type = 'desc', offset = 0) {
     var data = {
       offset: offset,
       order_column: order_column,
@@ -15,50 +19,173 @@ class AjaxHandler {
       class_catalog_id: cat_id
     };
 
-    return postRequest('https://test7api.baodao7.com/web/classes/list', data);
+    var ret_array = [];
+    // var courseBoxes = this.courseBoxes;
+    await this.postRequest(this.classesUrl, data).then(response => {
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        var id = data[i].id;
+        var title = data[i].name;
+        var date_ym = data[i].created_at.split("-")[0] + data[i].created_at.split("-")[1];
+        var thumbnail_url = "https://image.baodao7.com/upload/" + date_ym + "/" + data[i].picture_filename;
+        var updated_at = data[i].updated_at;
+        var teacher_id = data[i].teacher_id;
+        var cat_id = data[i].class_catalog_id;
+
+        ret_array.push({
+          id: id,
+          cat_id: cat_id,
+          title: title,
+          thumbnail_url: thumbnail_url,
+          teacher_id: teacher_id,
+          updated_at: updated_at
+        });
+      }
+    })
+
+    return ret_array;
   }
 
-  // 取得該集資料
-  getLession(id) {
-    return postRequest('https://test7api.baodao7.com/web/classes', {
+  // 取得該課程資料
+  getClass(id) {
+    var ret_array = [];
+
+    this.postRequest(this.classUrl, {
       id: id
+    }).then(response => {
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        // console.log(data[i].id);
+        var id = data[i].id;
+        var title = data[i].name;
+        var content = data[i].content;
+        var teacher_id = data[i].teacher_id;
+        var updated_at = data[i].updated_at;
+        var price = Number(data[i].price_market);
+
+        ret_array.push({
+          id: id,
+          title: title,
+          content: content,
+          teacher_id: teacher_id,
+          updated_at: updated_at,
+          price: price
+        });
+      }
     });
+
+    return ret_array;
   }
 
   // 取得所有集數資料
   // param class_id: 需求課程之id *必填*
-  getLessions(class_id, order_column = id, order_type = 'desc', offset = 0) {
+  getLessons(class_id, order_column = id, order_type = 'desc', offset = 0) {
     var data = {
       class_id: class_id,
       order_column: order_column,
       order_type: order_type,
-      offset: offset,
+      offset: offset
     };
 
-    return postRequest('https://test7api.baodao7.com/web/class_details/list', data);
+    var ret_array = [];
+    this.postRequest(this.lessionsUrl, data).then(response => {
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        // console.log(data[i].id);
+        var id = data[i].id;
+        var title = data[i].info;
+        var content = data[i].content;
+
+        ret_array.push({
+          id: id,
+          title: title,
+          content: content,
+        });
+      }
+    });
+
+    return ret_array;
+  }
+
+  async getTotal(class_id) {
+    var data = {
+      class_id: class_id,
+    };
+
+    var total = null;
+    await this.postRequest(this.lessionsUrl, data).then(response => {
+      total = response.total;
+    });
+
+    return total;
   }
 
   // 取得該集資料
-  getLession(id) {
-    return postRequest('https://test7api.baodao7.com/web/class_details', {
+  getLesson(id) {
+    var ret_array = [];
+    this.postRequest(this.lessionUrl, {
       id: id
+    }).then(response => {
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        // console.log(data[i].id);
+        var id = data[i].id;
+        var title = data[i].info;
+        var content = data[i].content;
+        var filename = data[i].filename;
+        var updated_at = data[i].updated_at;
+        var price_market = data[i].price_market;
+
+        ret_array.push({
+          id: id,
+          title: title,
+          content: content,
+          filename: filename,
+          updated_at: updated_at,
+          price_market: price_market
+        });
+      }
     });
+
+    return ret_array;
   }
 
   // 取得老師資料
-  getTeacher(cat_id) {
-    return postRequest('https://test7api.baodao7.com/web/teacher', {
+  async getTeacher(cat_id) {
+    var ret_array = [];
+
+    await this.postRequest(this.teachersUrl, {
       class_catalog_id: cat_id
+    }).then(response => {
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        var id = data[i].id;
+        var name = data[i].name;
+        var date_ym = data[i].created_at.split("-")[0] + data[i].created_at.split("-")[1];
+        var picture_url = "https://image.baodao7.com/upload/" + date_ym + "/" + data[i].picture_filename;
+        var info = data[i].info;
+
+        ret_array.push({
+          id: id,
+          name: name,
+          picture_url: picture_url,
+          info: info
+        });
+      }
     });
+
+    return ret_array;
   }
 
   // request to api
   postRequest(url, data) {
     return fetch(url, {
-      body: Json.stringify(data),
-      cache: 'no-cache',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json'
+      },
       method: 'POST'
-    }).then(reponse => response.json()).catch(function(error) {
+    }).then(response => response.json()).catch(function(error) {
       console.log('There has been a problem: ', error.message);
     });
   }
--- js/course.js
+++ js/course.js
@@ -1,38 +1,59 @@
-class CourseHandler {
-  var courses = new AjaxHandler();
+class Course {
+  ajax = new AjaxHandler();
 
+  boxInfo = []; // stores array of courseBox object
+  boxStatus = false; // determinds whether courseBoxes is ready to use
   // get information needed to show in box
   // return json array or false if none found
   getClasses(cat_id = false) {
-    var response = courses.getClass(cat_id);
-    var ret_array = []; // array of json data of course info
-    if (response.success && response.data.length > 0) {
-      var data = response.data;
-      for (let i = 0; i < data.length; i++) {
-        var id = data[i].id;
-        var title = data[i].name;
-        var thumbnail = data[i].picture_filename;
-        var teacher_pic = getTeacher(data[i].class_catalog_id).profile_pic;
-        var lession_total = classLength(id);
+    var response = this.ajax.getClasses(cat_id);
 
-        ret_array.push({
-          id: id,
-          title: title,
-          thumbnail: thumbnail,
-          teacher_pic: teacher_pic,
-          lession_total: lession_total
-        });
+    var boxes = [];
+    var that = this;
+    response.then(data => {
+      for (var i in data) {
+        var courseBox = new CourseBox(data[i].id);
+        courseBox.cat_id = data[i].cat_id;
+        courseBox.title = data[i].title;
+        courseBox.thumbnail_url = data[i].thumbnail_url;
+        courseBox.teacher_id = data[i].teacher_id;
+        courseBox.updated_at = data[i].updated_at;
+        this.setTeacherUrl(courseBox);
+        this.setTotal(courseBox);
+
+        boxes.push(courseBox);
       }
+    }).then(() => {
+      that.boxInfo = boxes;
+      that.boxStatus = !that.boxStatus;
+      // this.showBoxes();
+    });
 
-      return ret_array;
-    }
+  }
 
-    return false;
+  // showBoxes() {
+  //   var sec = $("#box-sec");
+  //   var box = $('#box-template').clone();
+  //
+  // }
+
+  // 取得該課程教師圖片網址
+  setTeacherUrl(course) {
+    this.ajax.getTeacher(Number(course.cat_id)).then(data => {
+      course.teacher_url = data[0].picture_url;
+    });
+  }
+
+  // 取得該課程的集數
+  setTotal(course) {
+    this.ajax.getTotal(Number(course.id)).then(total => {
+      course.total = total;
+    });
   }
 
   // 取得老師資訊
   getTeacher(cat_id) {
-    var response = courses.getTeacher(cat_id);
+    var response = this.ajax.getTeacher(cat_id);
     if (response.success && response.data.length > 0) {
       var data = response.data[0];
 
@@ -52,12 +73,27 @@ class CourseHandler {
 
   // 取得課程集數
   classLength(class_id) {
-    var response = courses.getLessions(class_id);
+    var response = this.ajax.getLessions(class_id);
     if (response.success && response.data.length > 0) {
       return reponse.total;
     }
 
     return false;
+  }
+
+}
+
+class CourseBox {
+  id = null;
+  title = "";
+  thumbnail_url = "";
+  teacher_id = "";
+  teacher_url = "";
+  total = 0;
+  updated_at = "";
+
+  constructor(class_id) {
+    this.id = class_id;
   }
 
 }
