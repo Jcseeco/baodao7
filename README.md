d606b55077b49cb75ab92f6991f1fdc705bd5118
jcseeco <dontbirdanyone@gmail.com>
2020-04-17 13:32:14 +0800

    觀看頁改讀取url設定課程id

--- README.md
+++ README.md
@@ -5,9 +5,7 @@
 
 ### TODO
 * 登入與購買驗證
-* 觀看頁以讀取cookie方式取得課程id
 * 換頁標題顯示
 * 將section分檔成view管理，改為使用php template方式
-* 換頁標題顯示
 
 link to test site: [寶島奇蹟AMB](https://baodao7-mail.com)
--- js/watch.js
+++ js/watch.js
@@ -8,11 +8,16 @@ function watch() {
     lesson_price: 123, // 當前分集價格
     class_price: null, // 整體課程價格
     lessons: [], // 所有分集資料
-    init() {},
+    init() {
+      const queryString = location.search;
+      const urlParams = new URLSearchParams(queryString);
+      // set class if url has class_id
+      (urlParams.has('class_id')) ? this.setClass(urlParams.get('class_id')): null;
+
+    },
     setClass(id) {
       this.class_id = id;
       this.setLessons(id);
-      this.lesson_title = "test";
     },
     async setLessons(class_id) {
       var lessons = await this.ajax.getLessons(class_id).then(data => {
